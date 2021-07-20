57c334664d29d10de0f3f838921fbc7ac77afe16
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-07-20 01:43:06 -0300

    redirect to stripe checkout

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -7,7 +7,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST') {
 
     const session = await getSession({ req });
-
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email
     })
@@ -25,8 +24,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       mode: 'subscription',
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
-      cancel_url: process.env.STRIPE_CANCEL_URL,
-      
+      cancel_url: process.env.STRIPE_CANCEL_URL
     })
 
     return res.status(200).json({ sessionId: stripeCheckoutSession.id })
